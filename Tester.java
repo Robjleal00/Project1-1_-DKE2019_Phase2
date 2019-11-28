bd0eb87b423209a1ee7543b3bcfdaa96ac75ee57
Иван Поляков <ivan.polyakov.01@gmail.com>
2019-11-28 19:40:44 +0100

    cleared

--- Tester.java
+++
@@ -1,9 +0,0 @@
-public class Tester {
-	public static final int DEFAULT_CELL = 50;
-	public static UI ui;
-
-	public static void main(String[] args) {
-		int a = RandomEngine.getInt(0, 12);
-		System.out.println(a);
-	}
-}
